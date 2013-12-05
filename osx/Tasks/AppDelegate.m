a9c71fa7ba310ca40867301ba34b9c41161748e3
Ринат Муртазин <murtazin13@gmail.com>
2013-12-05 21:12:07 +0600

    fix bug and refactoring

--- osx/Tasks/AppDelegate.m
+++ osx/Tasks/AppDelegate.m
@@ -12,15 +12,15 @@
 
 @interface AppDelegate () <NSTableViewDataSource, NSTableViewDelegate, NSTextFieldDelegate>
 
-@property (nonatomic, readonly) DBAccountManager *accountManager;
-@property (nonatomic, readonly) DBAccount *account;
+@property(nonatomic, readonly) DBAccountManager *accountManager;
+@property(nonatomic, readonly) DBAccount *account;
 
-@property (nonatomic, retain) DBDatastore *store;
-@property (nonatomic, retain) NSMutableArray *tasks;
-@property (nonatomic, retain) NSTimer *clipboardTimer;
+@property(nonatomic, retain) DBDatastore *store;
+@property(nonatomic, retain) NSMutableArray *tasks;
+@property(nonatomic, retain) NSTimer *clipboardTimer;
 
-@property (nonatomic, retain) NSString* toPut;
-@property (nonatomic, assign) BOOL firstTime;
+@property(nonatomic, retain) NSData *oldObject;
+@property(nonatomic, assign) BOOL firstTime;
 @end
 
 
@@ -36,66 +36,54 @@
     }];
     [self setupTasks];
 
-    [NSApp setActivationPolicy: NSApplicationActivationPolicyAccessory];
-    
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
+
     _clipboardTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerHandler) userInfo:NULL repeats:YES];
-    
+
 
 }
 
-- (void) timerHandler {
+- (void)timerHandler {
     NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
     NSArray *classes = [[NSArray alloc]
-                        initWithObjects:
-                        [NSString class],
-                        [NSImage class],
-                        nil];
+            initWithObjects:
+                    [NSString class],
+                    [NSImage class],
+                    nil];
     NSDictionary *options = [NSDictionary dictionary];
     NSArray *copiedItems = [pasteboard readObjectsForClasses:classes options:options];
 
     if (copiedItems != nil && [self.account isLinked]) {
-        
+
+
         //todo: добавить проверку что объекты не идентичны (чтобы не добавлять повторно)
-        
-        NSObject* obj = [copiedItems objectAtIndex:0];
+
+        NSObject *obj = [copiedItems objectAtIndex:0];
+
+
+        if (![self isNewObject:obj]) {
+
+            return;
+        }
+
         if ([obj isKindOfClass:[NSImage class]]) {
-            NSImage *img = (NSImage*) obj;
-            
-            NSBitmapImageRep *imgRep = [[img representations] objectAtIndex: 0];
-            NSData *data = [imgRep representationUsingType: NSPNGFileType properties: nil];
-            
-            unsigned char hash[16];
-            CC_MD5([data bytes], [data length], hash);
-            
-            NSString *imageHash = [NSString stringWithFormat:
-                                   @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-                                   hash[0], hash[1], hash[2], hash[3],
-                                   hash[4], hash[5], hash[6], hash[7],
-                                   hash[8], hash[9], hash[10], hash[11],
-                                   hash[12], hash[13], hash[14], hash[15]
-                                   ];
-            
-            if ([imageHash isEqualToString:self.toPut]) {
-                return;
-            }
-            
-            self.toPut = imageHash;
-            if (self.firstTime) {
-                self.firstTime = NO;
-                return;
-            }
-            
-            NSString *dateString = [NSDateFormatter localizedStringFromDate: [[NSDate alloc] init]
-                                                                  dateStyle: NSDateFormatterShortStyle
-                                                                  timeStyle: NSDateFormatterShortStyle];
+
+            NSImage *img = (NSImage *) obj;
+
+            NSBitmapImageRep *imgRep = [[img representations] objectAtIndex:0];
+            NSData *data = [imgRep representationUsingType:NSPNGFileType properties:nil];
+
+            NSString *dateString = [NSDateFormatter localizedStringFromDate:[[NSDate alloc] init]
+                                                                  dateStyle:NSDateFormatterShortStyle
+                                                                  timeStyle:NSDateFormatterMediumStyle];
 
             NSString *shotAt = @"Shot at ";
-            NSString *tmpFileName =[[shotAt stringByAppendingString:dateString] stringByAppendingString:@".png"];
+            NSString *tmpFileName = [[shotAt stringByAppendingString:dateString] stringByAppendingString:@".png"];
 
             NSString *tmpFilepath = @"/tmp/";
-            [data writeToFile: [tmpFilepath stringByAppendingString: tmpFileName]
-                   atomically: NO];
-            
+            [data writeToFile:[tmpFilepath stringByAppendingString:tmpFileName]
+                   atomically:NO];
+
             DBFilesystem *filesystem = [DBFilesystem sharedFilesystem];
             if (!filesystem) {
                 filesystem = [[DBFilesystem alloc] initWithAccount:self.account];
@@ -104,55 +92,90 @@
             DBError *error = nil;
 
             DBPath *path = [[DBPath root] childPath:tmpFileName];
-            NSLog(@"%@", path.stringValue);
+
             if (![filesystem fileInfoForPath:path error:&error]) { // see if path exists
-                
-                NSLog(@"%@", path);
+
                 // Report error if path look up failed for some other reason than NOT FOUND
                 if ([error code] != DBErrorParamsNotFound) {
                     NSLog(@"Error");
                 }
-                
+
                 // Create a new test file.
                 DBFile *file = [[DBFilesystem sharedFilesystem] createFile:path error:&error];
                 if (!file) {
                     NSLog(@"Error");
                 }
-                
+
                 // Write to the new test file.
                 if (![file writeData:data error:&error]) {
                     NSLog(@"Error");
                 }
-                
+
                 [file close];
-                
+
                 DBTable *tasksTbl = [self.store getTable:BUFS_TABLE];
-                
-                DBRecord *buf = [tasksTbl insert:@{@"value": path,
-                                                   @"type": @"image",
-                                                   @"created": [NSDate date] } ];
+
+                DBRecord *buf = [tasksTbl insert:@{@"value" : tmpFileName,
+                        @"type" : @"image",
+                        @"created" : [NSDate date]}];
                 [_tasks addObject:buf];
-                
+
             } else {
                 NSLog(@"Error");
             }
-            
-        } else if ([obj isKindOfClass:[NSString class]]) {
-            NSString *string = (NSString *) obj;
-            
-            if ([string isEqualToString:self.toPut]) {
-                return;
-            }
 
-            self.toPut = string;
-            
+        } else if ([obj isKindOfClass:[NSString class]]) {
+
+            NSString *string = (NSString *) obj;
+
             DBTable *tasksTbl = [self.store getTable:BUFS_TABLE];
-            DBRecord *buf = [tasksTbl insert:@{@"value": self.toPut,
-                                               @"type": @"plain",
-                                               @"created": [NSDate date] } ];
+            DBRecord *buf = [tasksTbl insert:@{@"value" : string,
+                    @"type" : @"plain",
+                    @"created" : [NSDate date]}];
             [_tasks addObject:buf];
         }
     }
+}
+
+- (BOOL)isNewObject:(NSObject *)object {
+
+    NSData *data;
+
+    if ([object isKindOfClass:[NSImage class]]) {
+
+        NSImage *img = (NSImage *) object;
+
+        data = [img TIFFRepresentation];
+
+    } else if ([object isKindOfClass:[NSString class]]) {
+
+        NSString *string = (NSString *) object;
+
+        data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    }
+
+    if ([_oldObject isEqualToData:data]) {
+
+        return NO;
+
+    } else {
+
+        self.oldObject = data;
+
+        return ([self isFirstTime] ? NO : YES );
+    }
+}
+
+- (BOOL)isFirstTime {
+
+    if (self.firstTime) {
+
+        self.firstTime = NO;
+
+        return YES;
+    }
+
+    return NO;
 }
 
 
@@ -166,18 +189,18 @@
     }
 }
 
--(id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
     return nil;
 }
 
 #pragma mark - NSTableViewDelegate
 
--(NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
     if (![DBAccountManager sharedManager].linkedAccount) {
         return [tableView makeViewWithIdentifier:@"LinkCell" owner:self];
     } else if (row == [_tasks count]) {
         return [tableView makeViewWithIdentifier:@"InputTaskCell" owner:self];
-    } else if (row == [_tasks count]+1) {
+    } else if (row == [_tasks count] + 1) {
         return [tableView makeViewWithIdentifier:@"UnlinkCell" owner:self];
     } else {
         TaskCellView *taskCell = [tableView makeViewWithIdentifier:@"TaskCell" owner:self];
@@ -188,7 +211,7 @@
     }
 }
 
--(CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row {
+- (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row {
     if (self.account) {
         if (row == [_tasks count] + 1) {
             return 70.0;
@@ -204,6 +227,7 @@
 - (IBAction)didPressLink:(id)sender {
     [[DBAccountManager sharedManager] linkFromWindow:[self window] withCompletionBlock:nil];
 }
+
 - (IBAction)didPressUnlink:(id)sender {
     [[[DBAccountManager sharedManager] linkedAccount] unlink];
     self.store = nil;
@@ -236,9 +260,9 @@
 
     DBTable *bufsTbl = [self.store getTable:BUFS_TABLE];
 
-    NSArray * records= [bufsTbl query:nil error:nil];
+    NSArray *records = [bufsTbl query:nil error:nil];
 
-    for(DBRecord *record in records) {
+    for (DBRecord *record in records) {
 
         [record deleteRecord];
     }
@@ -250,7 +274,7 @@
     if (self.account && [textField.stringValue length]) {
         DBTable *tasksTbl = [self.store getTable:@"tasks"];
 
-        DBRecord *task = [tasksTbl insert:@{ @"taskname": textField.stringValue, @"completed": @NO, @"created": [NSDate date] } ];
+        DBRecord *task = [tasksTbl insert:@{@"taskname" : textField.stringValue, @"completed" : @NO, @"created" : [NSDate date]}];
         [_tasks addObject:task];
         [self.tableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:[_tasks count] - 1] withAnimation:NSTableViewAnimationEffectFade];
 
@@ -279,13 +303,13 @@
 - (void)setupTasks {
     if (self.account) {
         __weak AppDelegate *slf = self;
-        [self.store addObserver:self block:^ {
+        [self.store addObserver:self block:^{
             if (slf.store.status & (DBDatastoreIncoming | DBDatastoreOutgoing)) {
                 [slf syncTasks];
             }
         }];
         _tasks = [NSMutableArray arrayWithArray:[[self.store getTable:@"tasks"] query:nil error:nil]];
-        [_tasks sortUsingComparator: ^(DBRecord *obj1, DBRecord *obj2) {
+        [_tasks sortUsingComparator:^(DBRecord *obj1, DBRecord *obj2) {
             return [obj1[@"created"] compare:obj2[@"created"]];
         }];
     } else {
@@ -328,7 +352,7 @@
 
     // Add new rows (in sorted order assuming _tasks is already sorted)
     [_tasks addObjectsFromArray:[changed allValues]]; // anything not processed in changed are inserts
-    [_tasks sortUsingComparator: ^(DBRecord *obj1, DBRecord *obj2) {
+    [_tasks sortUsingComparator:^(DBRecord *obj1, DBRecord *obj2) {
         return [obj1[@"created"] compare:obj2[@"created"]];
     }];
     NSIndexSet *inserts = [_tasks indexesOfObjectsPassingTest:^BOOL(DBRecord *obj, NSUInteger idx, BOOL *stop) {
