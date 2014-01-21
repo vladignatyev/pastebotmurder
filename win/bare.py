2788d5951ad2002e9dd71d2ca454f67dd821d78a
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2014-01-21 20:46:28 +0400

    Web UI and button handler

--- win/bare.py
+++ win/bare.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python	
 import wx
+from wx.webkit import WebKitCtrl
 
 class CustomTaskBarIcon(wx.TaskBarIcon):
 	ID_HELLO = wx.NewId()
@@ -35,13 +36,22 @@ class CustomTaskBarIcon(wx.TaskBarIcon):
 class ShotBufFrame(wx.Frame):
 
 	def __init__(self, parent, id, title):
-		wx.Frame.__init__(self, parent, -1, title, size=(1,1), style=wx.FRAME_NO_TASKBAR|wx.NO_FULL_REPAINT_ON_RESIZE)
+		wx.Frame.__init__(self, parent, -1, title, size=(410,290))
+
+		self.panel = wx.Panel(self)
+		button = wx.Button(self.panel, label="Connect now", pos=(130,200), size=(140,50))
+
+		self.Bind(wx.EVT_BUTTON, self.OnConnectDropbox, button)
 		
+		self.web = WebKitCtrl(self.panel, 4030, 'http://google.com', (5,5), (200,200))
+		print self.web
 		self.tbiicon = CustomTaskBarIcon()
 		self.Show()
 
+	def OnConnectDropbox(self, event):
+		wx.MessageBox('Connected Dropbox', 'Info', wx.OK | wx.ICON_INFORMATION)
 if __name__ == '__main__':
 	app = wx.App(False)
-	frame = ShotBufFrame(None, -1, ' ')
-	frame.Show(False)
+	frame = ShotBufFrame(None, -1, 'ShotBuf')
+	frame.Show(True)
 	app.MainLoop()
