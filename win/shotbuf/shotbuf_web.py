646d7ab93af0ce00b4414abcc6328a7d32d160b1
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2014-02-05 06:49:22 +0400

    win: cleanup

--- win/shotbuf/shotbuf_web.py
+++ win/shotbuf/shotbuf_web.py
@@ -38,9 +38,14 @@ def init_db():
     """Creates the database tables."""
     with app.app_context():
         db = get_db()
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
+        db.cursor().executescript("""CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  access_token BLOB
+);
+""")
         db.commit()
+        
 
 
 def get_db():
